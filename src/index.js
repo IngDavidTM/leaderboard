d0a966d657963c5dd9d288220f0e250b2d54c9cb
IngDavidTM <david-1011@hotmail.com>
2022-10-12 17:40:15 -0500

    improve UX

--- src/index.js
+++ src/index.js
@@ -3,20 +3,30 @@ import './index.css';
 const user = document.getElementById('inputName');
 const score = document.getElementById('inputScore');
 const submitBtn = document.getElementById('submitBtn');
+const error = document.getElementById('error');
 
 const addName = async (user, score) => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DkwEJKDtxGiSeljdVMZh/scores/', {
-    method: 'POST',
-    body: JSON.stringify({
-      user,
-      score,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  document.getElementById('inputName').value = '';
-  document.getElementById('inputScore').value = '';
+  if (user === '' || score === '') {
+    error.innerHTML = 'Please fill all the required fields';
+    setTimeout(() => { error.innerHTML = ''; }, 3000);
+  } else {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4tfml0QLmgKTloHBYIf5/scores/', {
+      method: 'POST',
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response.status !== 201) {
+      error.innerHTML = 'Something went wrong';
+      setTimeout(() => { error.innerHTML = ''; }, 3000);
+    }
+    document.getElementById('inputName').value = '';
+    document.getElementById('inputScore').value = '';
+  }
 };
 
 submitBtn.addEventListener('click', (e) => {
@@ -27,7 +37,7 @@ submitBtn.addEventListener('click', (e) => {
 const table = document.getElementById('table');
 const refresh = document.getElementById('refresh');
 const setName = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DkwEJKDtxGiSeljdVMZh/scores/');
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4tfml0QLmgKTloHBYIf5/scores/');
   const json = await response.json();
   const jsonSort = json.result.sort((a, b) => b.score - a.score);
   table.innerHTML = '';
@@ -41,6 +51,7 @@ const setName = async () => {
   });
 };
 
-refresh.addEventListener('click', setName);
-
-setName();
+refresh.addEventListener('click', () => {
+  refresh.innerHTML = 'Refresh';
+  setName();
+});
