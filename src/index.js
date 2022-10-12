d1592ddfdb331c9d8da262485c7f332b4d1d7843
IngDavidTM <david-1011@hotmail.com>
2022-10-12 16:51:30 -0500

    add refresh function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const score = document.getElementById('inputScore');
 const submitBtn = document.getElementById('submitBtn');
 
 const addName = async (user, score) => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fxeQ6enD2b5df5nFHNAr/scores/', {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DkwEJKDtxGiSeljdVMZh/scores/', {
     method: 'POST',
     body: JSON.stringify({
       user,
@@ -25,18 +25,22 @@ submitBtn.addEventListener('click', (e) => {
 });
 
 const table = document.getElementById('table');
+const refresh = document.getElementById('refresh');
 const setName = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fxeQ6enD2b5df5nFHNAr/scores/');
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DkwEJKDtxGiSeljdVMZh/scores/');
   const json = await response.json();
-  json.result.forEach((game, index) => {
+  const jsonSort = json.result.sort((a, b) => b.score - a.score);
+  table.innerHTML = '';
+  jsonSort.forEach((game, index) => {
     const tr = document.createElement('tr');
     tr.classList = (index % 2) ? 'gray' : '';
     table.appendChild(tr);
     const td = document.createElement('td');
-    td.innerHTML = `${game.user}:  ${game.score}`;
+    td.innerHTML = `${game.user}:   ${game.score}`;
     tr.appendChild(td);
   });
-  console.log(objName);
 };
+
+refresh.addEventListener('click', setName);
 
 setName();
